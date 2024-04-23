9536b6bbdcf10dcc41070595f7f1f5bc92d1b128
cyran <gs.gouin@gmail.com>
2024-04-23 20:28:35 +0200

    mise en place menu responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,14 @@ import {map, Observable} from "rxjs";
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
+  public appPages = [
+    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+  ];
   isLoggedIn$: Observable<boolean>;
   isMeetingPage: boolean = false;
   isLoginPage: boolean = false;
